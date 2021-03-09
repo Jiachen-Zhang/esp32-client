c2ab3c072d6c9808cef232f6a10a2eecdb011039
Jiachen <zhangjc1999@gmail.com>
2021-03-09 14:31:54 +0800

    [Feature] 使用Pipe实现读写进程分离，提高在树莓派上的数据吞吐量

--- data_uploader.py
+++ data_uploader.py
@@ -4,7 +4,6 @@
 import time
 import requests
 from utils.logger import log
-from utils.channel import SERIAL_QUEUE
 from serial_reader import SerialReader
 
 
@@ -14,11 +13,13 @@ def mock_data(_line: str, num: int) -> list:
     """
     _data = []
     for _ in range(num):
-        _data.append(_line + ", "+ str(int(time.time()*1000)))
+        _data.append(_line + ", " + str(int(time.time() * 1000)))
     return _data
+
 
 TEST_URL = 'https://service-mpkbqc6a-1256776799.gz.apigw.tencentcs.com/test/esp_collector'
 RELEASE_URL = 'https://service-mpkbqc6a-1256776799.gz.apigw.tencentcs.com/release/esp_collector'
+
 
 def post_data(_data: list, post_url=RELEASE_URL):
     """
@@ -27,32 +28,27 @@ def post_data(_data: list, post_url=RELEASE_URL):
     _r = requests.post(url=post_url, json=_data)
     log.info("send %d at %f", _r.status_code, time.time())
 
-# if __name__ == '__main__':
-#     data = mock_data('CSI_DATA,AP,A8:03:2A:68:69:BC,-57,11,1,5,1,1,1,1,0,0,0,-91,1,8,1, \
-#         5589939,0,84,0,0,5.782990,384,[84 -64 4 0 0 0 0 0 0 0 0 0 0 24 -1 24 -1 24 -3 24 \
-#             -3 24 -4 24 -4 23 -5 23 -5 23 -5 23 -5 22 -5 22 -6 21 -6 21 -6 21 -6 20 -6 \
-#                 19 -6 19 -5 19 -5 19 -5 19 -5 18 -5 18 -4 18 -3 17 -4 17 0 0 -3 16 -3 \
-#                     16 -2 16 -2 16 -1 15 -1 16 0 15 0 15 0 15 0 15 1 14 1 14 2 14 2 14 \
-#                         2 14 2 13 3 13 3 13 4 13 4 13 4 12 4 12 5 12 5 11 6 11 5 11 0 \
-#                             0 0 0 0 0 0 0 0 0 ]', 1000)
-#     start = time.time()
-#     post_data(data, TEST_URL)
-#     print(time.time() - start)
+
 if __name__ == '__main__':
-    import _thread
-    SERIAL_PORT = '/dev/tty.usbserial-0001'
-    BAUD_RATE = 921600
-    serial_reader = SerialReader(_serial_port=SERIAL_PORT, _baud_rate=BAUD_RATE)
-    _thread.start_new_thread(serial_reader.assembly_serial_data, ())
+    from multiprocessing.connection import Pipe
+    from multiprocessing import Process
+
+    SERIAL_PIPE = Pipe()
+    serial_reader = SerialReader(_serial_pipe_sender=SERIAL_PIPE[0],
+                                 _serial_port='/dev/tty.usbserial-0001',
+                                 _baud_rate=921600)
+    p = Process(target=serial_reader.assembly_serial_data,
+                args=())
+    p.start()
     data = []
     MAX_LEN = 1024
     while True:
-        line = SERIAL_QUEUE.get()
+        line = SERIAL_PIPE[1].recv()
         # remove \r\n
         data.append(line.strip())
-        _len = len(data)
-        if _len >= MAX_LEN:
+        LEN = len(data)
+        if LEN >= MAX_LEN:
             post_data(data)
             data = []
-        elif _len % 100 == 0:
-            log.info('%d/%d', _len, MAX_LEN)
+        elif LEN % 100 == 0:
+            log.info('%d/%d', LEN, MAX_LEN)
--- serial_reader.py
+++ serial_reader.py
@@ -4,6 +4,7 @@
 """
 import time
 from queue import Full
+from multiprocessing.connection import Connection
 from serial import LF, Serial
 from utils.logger import log
 from utils.channel import SERIAL_QUEUE
@@ -14,15 +15,24 @@ class SerialReader:
     串口读取对象
     """
 
-    def __init__(self, _serial_port: str = '/dev/tty.USB0', _baud_rate: int = 115200):
-        self.serial = Serial(_serial_port, _baud_rate, timeout=3)
-        self.serial.flushInput()
+    def __init__(self,
+                 _serial_pipe_sender: Connection = None,
+                 _serial_port: str = '/dev/ttyUSB0',
+                 _baud_rate: int = 115200, ):
+        self.serial_pipe_sender = _serial_pipe_sender
+        self._serial_port = _serial_port
+        self._baud_rate = _baud_rate
+        self.serial = None
 
     def reset(self):
         """
         刷新串口数据
         :return:
         """
+        self.serial.flushInput()
+
+    def __connect(self):
+        self.serial = Serial(self._serial_port, self._baud_rate, timeout=3)
         self.serial.flushInput()
 
     @staticmethod
@@ -41,30 +51,52 @@ class SerialReader:
         _data += ', {}\n'.format(time.time())
         return _data
 
+    def __read_serial_data(self):
+        serial_data = self.serial.read_until(expected=LF, size=4096)
+        # remove /r/n
+        serial_data = self.__append_time(serial_data)
+        if not serial_data.startswith('CSI_DATA'):
+            log.info(serial_data)
+        return serial_data
+
     def assembly_serial_data(self):
         """
         按行组装串口数据并发送至管道
         :return:
         """
-        while True:
-            serial_data = self.serial.read_until(expected=LF, size=4096)
-            # remove /r/n
-            serial_data = self.__append_time(serial_data)
-            # print(serial_data, end='')
-            if serial_data == '' or not serial_data.startswith('CSI_DATA'):
-                continue
-            assert serial_data.endswith('\n')
-            try:
-                SERIAL_QUEUE.put_nowait(serial_data)
-            except Full:
-                log.warning('Write Failed to SERIAL_QUEUE [Full]')
+        self.__connect()
+        if self.serial_pipe_sender is None:
+            while True:
+                serial_data = self.__read_serial_data()
+                if serial_data == '' or not serial_data.startswith('CSI_DATA'):
+                    continue
+                assert serial_data.endswith('\n')
+                try:
+                    SERIAL_QUEUE.put_nowait(serial_data)
+                except Full:
+                    log.warning('Write Failed to SERIAL_QUEUE [Full]')
+        else:
+            while True:
+                serial_data = self.__read_serial_data()
+                if serial_data == '' or not serial_data.startswith('CSI_DATA'):
+                    continue
+                assert serial_data.endswith('\n')
+                self.serial_pipe_sender.send(serial_data)
 
 
 if __name__ == '__main__':
-    import _thread
-    SERIAL_PORT = '/dev/tty.usbserial-0001'
-    BAUD_RATE = 921600
-    serial_reader = SerialReader(_serial_port=SERIAL_PORT, _baud_rate=BAUD_RATE)
-    _thread.start_new_thread(serial_reader.assembly_serial_data, ())
-    while data := SERIAL_QUEUE.get():
-        print('READ: {}'.format(data), end='')
+    from multiprocessing.connection import Pipe
+    from multiprocessing import Process
+    SERIAL_PIPE = Pipe()
+    serial_reader = SerialReader(_serial_pipe_sender=SERIAL_PIPE[0],
+                                 _serial_port='/dev/tty.usbserial-0001',
+                                 _baud_rate=921600)
+    p = Process(target=serial_reader.assembly_serial_data,
+                args=())
+    p.start()
+    data = []
+    MAX_LEN = 1024
+    while True:
+        line: str = SERIAL_PIPE[1].recv()
+        line = line.rstrip()
+        print('READ: {}'.format(line))
